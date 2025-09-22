52645a025cc0ecf94d3e376462c9ecea5bba9d18
keagan <developerkeagan@gmail.com>
2025-09-22 14:06:47 +0100

    2nd commit: United For Gaza website

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { networkInterfaces } from 'os';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Listen on all network interfaces
-    port: 5173, // You can change this to your desired port
-  }
+  server: { https: false }
 })
