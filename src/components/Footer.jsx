d06b1e911478a2d7a0a4aca3ba0891acea6a95f1
keagan <developerkeagan@gmail.com>
2025-09-23 20:55:55 +0100

    helo bitches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 // --- Component-level Constants for easier maintenance ---
-const FOOTER_LINKS = [
-    { name: 'Home', href: '/' },
-    { name: 'About Us', href: '/about' },
-    { name: 'Gallery', href: '/gallery' },
-    { name: 'Our Impact', href: '/ourimpact' },
-];
-
 const SOCIAL_ICONS = [
     { name: 'facebook', href: '#' },
     { name: 'twitter', href: '#' },
@@ -24,9 +17,10 @@ const Footer = () => {
           <div className="col-lg-12 col-sm-12 col-xs-12">
             <div className="footer_menu">
               <ul>
-                {FOOTER_LINKS.map(link => (
-                    <li key={link.name}><Link to={link.href}>{link.name}</Link></li>
-                ))}
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="/gallery">Gallery</Link></li>
+                <li><Link to="/ourimpact">Our Impact</Link></li>
               </ul>
             </div>
             <div className="footer_copyright">
